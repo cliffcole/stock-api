a54ee38db8d5775db2de03faec2979f8f576bf51
cliffcole <clifflcole@gmail.com>
2018-01-05 02:00:10 -0500

    updated configs

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const passportJwt = require('passport-jwt');
-const  models = require('./models')
+const models = require('./models')
 const sequelize = require('sequelize');
 const morgan = require('morgan');
 const jwt = require('jsonwebtoken');
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-//const ExtractJwt = passportJwt.ExtractJwt;
-//const JwtStrategy = passportJwt.Strategy;
+const config = require('./configs/config.json');
 
 const index = require('./routes/index');
 const login = require('./routes/login');
@@ -21,18 +20,11 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
+console.log(config);
 const jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-jwtOptions.secretOrKey = 'y0y0';
+jwtOptions.secretOrKey = config.secret;
 
-/* let strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
-    console.log('Payload recieved', jwt_payload);
-    models.Users.findOne({where: {username: username}}).then((user) => {
-        console.log(jwt_payload);
-        next();
-    })
-})
-passport.use(strategy); */
 passport.use(new JwtStrategy(jwtOptions, function(jwt_payload, done) {
     console.log("PAYLOAD: ", jwt_payload);
     models.Users.findOne({where: {id: jwt_payload.id}}).then(response => {
@@ -40,21 +32,6 @@ passport.use(new JwtStrategy(jwtOptions, function(jwt_payload, done) {
     });
 }));
 app.use(passport.initialize());
-/*passport.use(new LocalStrategy(
-    function(username, password, done) {
-        console.log("username: "+username);
-      models.Users.findOne({ where: { username: username }}).then((user) =>{
-        console.log(user);
-        done(null,user);
-      }) /* function (err, user) {
-          console.log(user);
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (!user.verifyPassword(password)) { return done(null, false); }
-        return done(null, user);
-      }); */
-   // }
-  //)); */
 
 app.use('/', index);
 app.use('/login', login);
